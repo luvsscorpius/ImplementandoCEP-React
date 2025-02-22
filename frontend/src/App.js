f224d083e9d64de1c42d9c8c5aa42e23fa1dbc84
and <andersonvittor1801@gmail.com>
2025-02-22 14:55:18 -0300

    feat(skeletons): implementing skeletons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,26 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import Skeleton from '@mui/material/Skeleton'
+import { Box } from '@mui/material'
 
 function App() {
   const [cep, setCep] = useState("")
   const [endereco, setEndereco] = useState({})
-  
+  const [loading, setLoading] = useState(false)
+
   const buscarCEP = async () => {
     if (cep.length === 8) {
-      try {
-        const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-        setEndereco(response.data)
-      } catch (error) {
-        console.error('Erro ao buscar o CEP', error)
-      }
+      setLoading(true)
+      setTimeout(async () => {
+        try {
+          const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+          setEndereco(response.data)
+        } catch (error) {
+          console.error('Erro ao buscar o CEP', error)
+        } finally {
+          setLoading(false)
+        }
+      }, 1000)
     }
   }
 
@@ -20,7 +28,15 @@ function App() {
     <>
       <label>Digite o CEP:</label>
       <input type='text' value={cep} onChange={(e) => setCep(e.target.value)} onBlur={buscarCEP} />
-      {endereco.logradouro && (
+
+      {loading ? (
+        <Box>
+          <Skeleton width={200} height={35} animation="wave" />
+          <Skeleton width={200} height={35} animation="wave" />
+          <Skeleton width={200} height={35} animation="wave" />
+          <Skeleton width={200} height={35} animation="wave" />
+        </Box>
+      ) : endereco.logradouro && (
         <div>
           <p>Rua: {endereco.logradouro}</p>
           <p>Bairro: {endereco.bairro}</p>
